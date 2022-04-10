20f5d29873fb9dcd8ac613fcedeb690399cba709
Leonard <lonardlator@yahoo.fr>
2022-04-10 17:17:32 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from 'web3uikit';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId="xxxxx" serverUrl="xxxxx">
+      <NotificationProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      </NotificationProvider>
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
